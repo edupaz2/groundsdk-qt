752cf15f57c05303f488f51888645f20cb46e035
Eduardo Paz <edupaz2@gmail.com>
2020-10-04 01:26:53 +0200

    Consume ManageGroundSdk

--- android/src/MainActivity.java
+++ android/src/MainActivity.java
@@ -2,12 +2,17 @@ package com.edupaz2.groundsdkqt;
 
 import org.qtproject.qt5.android.bindings.QtActivity;
 
+import com.parrot.drone.groundsdk.ManagedGroundSdk;
+
 import android.os.Bundle;
 
 public class MainActivity extends QtActivity {
 
+    private ManagedGroundSdk mGroundSdk;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mGroundSdk = ManagedGroundSdk.obtainSession(this);
     }
 }
