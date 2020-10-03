2dc3c4a4fd117820e0d12395fd006af671c21065
Eduardo Paz <edupaz2@gmail.com>
2020-10-04 01:19:59 +0200

    Custom MainActivity.java

---
+++ android/src/MainActivity.java
@@ -0,0 +1,13 @@
+package com.edupaz2.groundsdkqt;
+
+import org.qtproject.qt5.android.bindings.QtActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends QtActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
